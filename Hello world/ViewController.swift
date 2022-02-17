6106dc4891b2d5721035b9a5e3e54dcc33caa08f
Ilnaz <18sii08@mail.ru>
2022-02-17 06:20:27 +0300

    Initial Commit

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by Ильназ Салихов on 17.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
